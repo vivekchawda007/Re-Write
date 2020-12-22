5f95a2c98559c74a77a8f1de9e7bec7a187529c2
Vivek Chawda <vjcworld@gmail.com>
2020-12-22 17:58:46 +0530

    Changed Host

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:8081',
+  apiUrl: 'http://192.168.43.32:8080',
   secugenUrl : "https://localhost:8443/SGIFPCapture",
   expireTime: 6
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080',
+  apiUrl: 'http://192.168.43.32:8080',
   secugenUrl : "https://localhost:8443/SGIFPCapture",
   expireTime: 6
 };
