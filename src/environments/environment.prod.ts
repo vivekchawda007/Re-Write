a041c2a6b34fb23a4d7f5fb4513ee2f45f8331cf
Vivek Chawda <vjcworld@gmail.com>
2020-12-22 18:27:05 +0530

    Added https

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://192.168.43.32:8080',
+  apiUrl: 'https://192.168.43.32:443',
   secugenUrl : "https://localhost:8443/SGIFPCapture",
   expireTime: 6
 };
